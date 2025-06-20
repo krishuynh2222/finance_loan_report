df6b05f8e23814247a94b368e910d5084b48d9e1
Ngan Huynh <146058636+krishuynh2222@users.noreply.github.com>
2025-06-20 16:59:03 -0400

    Create 1_create_table.sql

---
+++ 1_create_table.sql
@@ -0,0 +1,29 @@
+USE [Finance Loan DB];
+DROP TABLE IF EXISTS dbo.financial_loan;
+
+CREATE TABLE dbo.financial_loan (
+   id INT,
+   address_state VARCHAR(50),
+   application_type VARCHAR(50),
+   emp_length VARCHAR(50),
+   emp_title VARCHAR(255),
+   grade VARCHAR(50),
+   home_ownership VARCHAR(50),
+   issue_date VARCHAR(50),
+   last_credit_pull_date VARCHAR(50),
+   last_payment_date VARCHAR(50),
+   loan_status VARCHAR(50),
+   next_payment_date VARCHAR(50),
+   member_id INT,
+   purpose VARCHAR(50),
+   sub_grade VARCHAR(50),
+   term VARCHAR(50),
+   verification_status VARCHAR(50),
+   annual_income INT,
+   dti REAL,
+   installment REAL,
+   int_rate REAL,
+   loan_amount INT,
+   total_acc INT,
+   total_payment INT
+);
